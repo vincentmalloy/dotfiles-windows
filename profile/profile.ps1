384f874a9be234a82f4a1b1cf624693d00eda220
Simon Lundius <simon@things-remote.com>
2024-05-14 10:22:40 +0200

    remove chocolatey

--- profile/profile.ps1
+++ profile/profile.ps1
@@ -1,12 +1,3 @@
 Push-Location ("$(Split-Path $profile.currentUserAllHosts)\imports")
 "aliases","functions","components" | Where-Object {Test-Path "$_.ps1"} | ForEach-Object -process {Invoke-Expression ". .\$_.ps1"}
 Pop-Location
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-# $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-# if (Test-Path($ChocolateyProfile)) {
-#   Import-Module "$ChocolateyProfile"
-# }
