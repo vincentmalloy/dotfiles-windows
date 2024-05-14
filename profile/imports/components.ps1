db334cee81e013310ed0b5f116aa2517e90c432c
Simon Lundius <simon@things-remote.com>
2024-05-14 10:55:24 +0200

    change omptheme address

--- profile/imports/components.ps1
+++ profile/imports/components.ps1
@@ -1,5 +1,5 @@
 # oh-my-posh
 if (Get-Command "oh-my-posh" -errorAction SilentlyContinue)
 {
-    oh-my-posh init pwsh --config "https://raw.githubusercontent.com/vincentmalloy/dotfiles/main/omptheme.json" | Invoke-Expression
+    oh-my-posh init pwsh --config "https://gist.githubusercontent.com/vincentmalloy/4b85151d28b3025451f5634b6081019d/raw/f40881c9a9381617c76d876f34f325f6be1010cc/omptheme.json" | Invoke-Expression
 }
