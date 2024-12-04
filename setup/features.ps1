7ef690bc747e7dd34f86de223ad69e21e103d7e5
Simon Lundius <25029432+vincentmalloy@users.noreply.github.com>
2024-12-04 11:33:11 +0100

    add nixos wsl install

--- setup/features.ps1
+++ setup/features.ps1
@@ -9,6 +9,34 @@ if($(Get-WindowsCapability -online | Where-Object -Property name -like "Media.Me
 # install wsl
 if(!(wsl -l)){
     wsl --set-default-version 2
-    wsl --install
+    wsl --install --no-distribution
     wsl --set-default-version 2
+}
+if(!(wsl -l | Where-Object {$_.Replace("`0","") -match '^NixOS'})){
+    $repo = "nix-community/NixOS-WSL"
+    $filename = "nixos-wsl.tar.gz"
+    
+    $download = "https://github.com/$repo/releases/latest/download/$filename"
+    
+    # Invoke-WebRequest $download -Out $filename
+
+    wsl --import NixOS $env:USERPROFILE\NixOS\ $filename --version 2
+    
+    # Remove-Item $filename
+    $hostname = "voyager2"
+    $githubUser = "vincentmalloy"
+    $flakeRepo = "nixos-config"
+    # $flakeFolder = "/mnt/c/Users/${env:USERNAME}/nix-config"
+    # $flake = "$flakeFolder#$hostname"
+    $flake = "github:/$githubUser/$flakeRepo#$hostname"
+    wsl -d NixOS sudo nixos-rebuild switch --flake $flake
+    wsl -t NixOS
+    # checkout flake and link it to /etc/nixos
+    wsl -d NixOS sudo rm -rf /etc/nixos
+    wsl -d NixOS git clone "https://github.com/$githubUser/$flakeRepo.git" ~/$flakeRepo
+    wsl -d NixOS sudo ln -s ~/$flakeRepo /etc/nixos
+    # wsl -d NixOS sudo ln -s $flakeFolder /etc/nixos
+    wsl -d NixOS sudo nixos-rebuild switch
+    wsl -t NixOS
+    wsl -s NixOS
 }
